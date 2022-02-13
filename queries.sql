04491cadfcb33fb76309375b1b211c2207962ef6
Krystal313 <kcheng313@gmail.com>
2022-02-13 17:27:07 -0500

    updated queries

--- queries.sql
+++ queries.sql
@@ -197,3 +197,97 @@ LEFT JOIN dept_emp
 ON current_emp.emp_no = dept_emp.emp_no
 WHERE dept_emp.to_date = ('9999-01-01')
 ORDER BY dept_emp.dept_no;
+
+-- To create a new table for general employee information, with their current salaries included
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- To join 3 tables together by filtering the salary of the eligible employee to date
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List of Department Retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Create a query that will return only the information of retirees relevant to the Sales team
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+d.dept_name
+INTO sales_info
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01')
+AND (d.dept_name = 'Sales');
+
+Select * from sales_info
+
+-- Create a query that will return only the information of retirees relevant to the Sales and Development teams
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+d.dept_name
+INTO sales_development_info
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01')
+-- to use the IN condition with the WHERE clause when creating two items in the same column.
+AND d.dept_name IN ('Sales', 'Development');
+
+Select * from sales_development_info
